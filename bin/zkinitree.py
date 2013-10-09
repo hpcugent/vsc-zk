521f46f1729682693a94a511b98a431f5110f15a
Kenneth Waegeman <kenneth.waegeman@ugent.be>
2013-10-09 16:21:42 +0200

    header

--- bin/zkinitree.py
+++ bin/zkinitree.py
@@ -1,4 +1,23 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+# -*- coding: latin-1 -*-
+#
+# Copyright 2013-2013 Ghent University
+#
+# This file is part of vsc-zk,
+# originally created by the HPC team of Ghent University (http://ugent.be/hpc/en),
+# with support of Ghent University (http://ugent.be/hpc),
+# the Flemish Supercomputer Centre (VSC) (https://vscentrum.be/nl/en),
+# the Hercules foundation (http://www.herculesstichting.be/in_English)
+# and the Department of Economy, Science and Innovation (EWI) (http://www.ewi-vlaanderen.be/en).
+#
+# All rights reserved.
+#
+#
+"""
+vsc-zk zkinitree
+
+@author: Kenneth Waegeman (Ghent University)
+"""
 
 from vsc.utils.generaloption import simple_option
 from kazoo.security import make_digest_acl
