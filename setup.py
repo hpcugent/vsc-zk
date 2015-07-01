125e238f9837b35c6b9c4b88532d90964eebce6f
kwaegema <kenneth.waegeman@ugent.be>
2015-07-01 16:25:12 +0200

    bump version

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ shared_setup.SHARED_TARGET.update({
 
 PACKAGE = {
     'name': 'vsc-zk',
-    'version': '0.6.3',
+    'version': '0.7.0',
     'author': [sdw],
     'maintainer': [sdw, kw],
     'packages': ['vsc', 'vsc.zk', 'vsc.zk.rsync'],
